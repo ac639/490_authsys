5f7320c6f14386bb5f8ffc7e098a364f7ca2b49b
ubuntu <ac639@njit.edu>
2019-03-10 15:47:38 -0400

    added registration

--- testRabbitMQClient2.php
+++ testRabbitMQClient2.php
@@ -36,3 +36,35 @@ function loginAuth($usrName, $usrPassword) {
 	echo $argv[0]." END".PHP_EOL;
 }
 
+function registerAuth($usrName,$usrPassword,$usrEmail) {
+        $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        if (isset($argv[1]))
+        {
+                $msg = $argv[1];
+        }
+        else
+        {
+                $msg = "test message";
+        }
+
+        $request = array();
+        //$request['type'] = "Login";
+        $request['type'] = "register";
+        //$request['username'] = "steve";
+        //$request['password'] = "password";
+        $request['usrName'] = $usrName;
+        $request['usrPassword'] = $usrPassword;
+	$request['usrEmail'] = $usrEmail;
+        $request['message'] = $msg;
+        $response = $client->send_request($request);
+        //$response = $client->publish($request);
+
+        echo "client received response: ".PHP_EOL;
+        print_r($response);
+        //return for test
+        return $response;
+        //
+        //echo "\n\n";
+
+        echo $argv[0]." END".PHP_EOL;
+}
