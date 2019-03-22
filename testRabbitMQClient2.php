173a0655d3df988f637956940918af95da94b202
root <ac639@njit.edu>
2019-03-21 23:33:49 -0400

    Added SQL data passthrough/fetch

--- testRabbitMQClient2.php
+++ testRabbitMQClient2.php
@@ -1,5 +1,5 @@
-#!/usr/bin/php
 <?php
+//#!/usr/bin/php
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -27,7 +27,7 @@ function loginAuth($usrName, $usrPassword) {
 	//$response = $client->publish($request);
 
 	echo "client received response: ".PHP_EOL;
-	print_r($response);
+	//print_r($response);
 	//return for test
 	return $response;
 	//
@@ -60,7 +60,7 @@ function registerAuth($usrName,$usrPassword,$usrEmail) {
         //$response = $client->publish($request);
 
         echo "client received response: ".PHP_EOL;
-        print_r($response);
+        //print_r($response);
         //return for test
         return $response;
         //
@@ -68,3 +68,39 @@ function registerAuth($usrName,$usrPassword,$usrEmail) {
 
         echo $argv[0]." END".PHP_EOL;
 }
+
+
+function fetchData($usrName, $sqlStatement) {
+        $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        if (isset($argv[1]))
+        {
+                $msg = $argv[1];
+        }
+        else
+        {
+                $msg = "test message";
+        }
+
+        $request = array();
+        //$request['type'] = "Login";
+        $request['type'] = "fetch";
+        //$request['username'] = "steve";
+        //$request['password'] = "password";
+        $request['usrName'] = $usrName;
+        $request['sqlStatement'] = $sqlStatement;
+        $request['message'] = $msg;
+        $response = $client->send_request($request);
+        //$response = $client->publish($request);
+
+        //echo "client received response: ".PHP_EOL;
+        //print_r($response);
+        //return for test
+        return $response;
+        
+        //echo "\n\n";
+
+        //echo $argv[0]." END".PHP_EOL;
+
+
+}
+
